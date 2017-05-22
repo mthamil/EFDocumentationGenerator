6a336b12ceb7cdac8832d90fe5bbdbfd91b08145
Matthew Hamilton <matthamilton@live.com>
2017-05-22 16:08:41 -0500

    Remove extra period.

--- EFDocumentationGenerator/ModelGenerationExtension.cs
+++ EFDocumentationGenerator/ModelGenerationExtension.cs
@@ -154,7 +154,7 @@ namespace DocumentationGenerator
             }
             catch (ConnectionStringLocationException exception)
             {
-                _logger.Log("Connection string could not be located for project '{0}': {1}.", project.Name, exception.Message);
+                _logger.Log("Connection string could not be located for project '{0}': {1}", project.Name, exception.Message);
                 if (mode == WizardKind.Generate)
                     _logger.Log("Try updating the model after initial generation and the connection string has been saved to a config file.");
 
