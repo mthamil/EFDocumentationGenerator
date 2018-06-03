a730477f6608653c4d05a3ddb2134608282be249
Matthew Hamilton <matthamilton@live.com>
2018-06-03 18:52:49 -0500

    Update example data model to include column with different name.

--- example/EFTest.DB/Items.sql
+++ example/EFTest.DB/Items.sql
@@ -3,7 +3,7 @@
 	[Id] int IDENTITY(1,1) NOT NULL PRIMARY KEY,
 	[Name] nvarchar(10) NOT NULL,
 	[Description] nvarchar(100) NOT NULL,
-	[IsEnabled] bit NOT NULL CONSTRAINT [DF_Items_IsEnabled] DEFAULT 'FALSE'
+	[IS_ENABLED] bit NOT NULL CONSTRAINT [DF_Items_IsEnabled] DEFAULT 'FALSE'
 )
 GO
 
@@ -13,7 +13,7 @@ GO
 EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'An item''s name.' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'Items', @level2type=N'COLUMN',@level2name=N'Name'
 GO
 
-EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Whether an item is enabled.' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'Items', @level2type=N'COLUMN',@level2name=N'IsEnabled'
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Whether an item is enabled.' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'Items', @level2type=N'COLUMN',@level2name='IS_ENABLED'
 GO
 
 EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Represents an item.' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'Items'
--- example/EFTest/EFTest.Designer.cs
+++ example/EFTest/EFTest.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'E:\Development\EFTest\EFTest\EFTest.edmx'. 
+﻿// T4 code generation is enabled for model 'E:\Development\EFDocumentationGenerator\example\EFTest\EFTest.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
